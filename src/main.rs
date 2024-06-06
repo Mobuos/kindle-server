8b8869ebdd28104bda619358aa3317867d6aa8ed
João Dini <copperbrjdmdini@gmail.com>
2024-06-06 17:15:10 -0300

    add basic tera templating example

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod image_converter_wrapper;
 use image_converter_wrapper as ic;
 mod kindle_manager_wrapper;
 use kindle_manager_wrapper as km;
+use rocket::Request;
 
 use std::fs;
 use std::path::Path;
@@ -10,22 +11,62 @@ use rocket::form::Form;
 use rocket::fs::{relative, FileServer, TempFile};
 use rocket::http::Status;
 
+use rocket_dyn_templates::{context, tera::Tera, Template};
+
 #[macro_use]
 extern crate rocket;
 
 #[derive(Debug, FromForm)]
-struct Upload<'v> {
+struct UploadImage<'v> {
     #[field(validate = len(1..=20))]
     filename: &'v str,
     // #[field(validate = supported_images())]
     file: TempFile<'v>,
 }
 
+#[get("/hello/<name>")]
+fn hello(name: &str) -> Template {
+    Template::render(
+        "hello",
+        context! {
+            title: "Hello",
+            name: Some(name),
+            items: vec!["One", "Two", "Three"],
+        },
+    )
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    Template::render(
+        "error/404",
+        context! {
+            uri: req.uri()
+        },
+    )
+}
+
+fn customize(tera: &mut Tera) {
+    tera.add_raw_template(
+        "about.html",
+        r#"
+        {% extends "base" %}
+
+        {% block content %}
+            <section id="about">
+              <h1>About - Here's another page!</h1>
+            </section>
+        {% endblock content %}
+    "#,
+    )
+    .expect("valid Tera template");
+}
+
 // FIXME: Returning Status directly is not recommended, see https://rocket.rs/guide/v0.5/responses/#responses
 //        Just doing this for now because me lazy
 // Maybe I will fix this after having more of an idea on what I'm supposed to go, and just go with it for now
 #[post("/", data = "<form>")]
-async fn submit<'r>(mut form: Form<Upload<'r>>) -> Status {
+async fn submit<'r>(mut form: Form<UploadImage<'r>>) -> Status {
     let filename = form.filename;
     println!("Filename: {}", form.filename);
     match form.file.persist_to(format!("images/{}", filename)).await {
@@ -89,5 +130,8 @@ fn rocket() -> _ {
     }
     rocket::build()
         .mount("/", routes![submit])
+        .mount("/", routes![hello])
         .mount("/", FileServer::from(relative!("/static")))
+        .register("/", catchers![not_found])
+        .attach(Template::fairing())
 }
