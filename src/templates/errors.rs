285a668f0d4fc0ab44a75aba5ce77b45a10a44cd
João Dini <copperbrjdmdini@gmail.com>
2024-11-04 16:51:53 -0300

    style: improve 404 styling and message

--- src/templates/errors.rs
+++ src/templates/errors.rs
@@ -2,10 +2,16 @@ use maud::{html, Markup};
 
 use super::elements::base;
 
-pub fn e404(uri: &str) -> Markup {
+pub fn e404(_uri: &str) -> Markup {
     let content = html! {
-        h1 { "404: Hey! There's nothing here." }
-        p { "The page at " (uri) " does not exist!"}
+        .py-8.px-4.mx-auto.max-w-screen-xl."lg:py-16"."lg:px-6" {
+            .mx-auto.max-w-screen-sm.text-center {
+                h1 .mb-4.text-7xl.tracking-tight.font-extrabold."lg:text-9xl".text-indigo-600 { "404" }
+                p .mb-4.text-3xl.font-bold.text-gray-900 { "There's nothing here :/" }
+                p .mb-4.text-lg.font-light.text-gray-500 { "Sorry, no page was found at this address, maybe go back to the home page?" }
+                //TODO: Link to homepage
+            }
+        }
     };
-    base("404 - Page not found", content)
+    base("404", content)
 }
