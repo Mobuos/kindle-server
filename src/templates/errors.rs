90ddaddcfeabc15f07d2f108cb162a8234645d6d
João Dini <copperbrjdmdini@gmail.com>
2024-11-04 17:25:53 -0300

    feat: add "Back to homepage" link to 404 error page

--- src/templates/errors.rs
+++ src/templates/errors.rs
@@ -9,7 +9,9 @@ pub fn e404(_uri: &str) -> Markup {
                 h1 .mb-4.text-7xl.tracking-tight.font-extrabold."lg:text-9xl".text-indigo-600 { "404" }
                 p .mb-4.text-3xl.font-bold.text-gray-900 { "There's nothing here :/" }
                 p .mb-4.text-lg.font-light.text-gray-500 { "Sorry, no page was found at this address, maybe go back to the home page?" }
-                //TODO: Link to homepage
+                a href="/"
+                    .btn-primary
+                    { "Back to homepage" }
             }
         }
     };
