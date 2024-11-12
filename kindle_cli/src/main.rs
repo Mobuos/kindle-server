142f0fbaabcc283724b8c908320d75ee484ee103
João Dini <copperbrjdmdini@gmail.com>
2024-11-12 19:01:12 -0300

    feat: initial remake of `kindle_manager` and `kindle_cli` as independent components
    Created `kindle_manager` and `kindle_cli` to replace the current bash scripts.
    This is an early implementation and is not yet integrated with `kindle_server`.

--- kindle_cli/src/main.rs
+++ kindle_cli/src/main.rs
@@ -1,5 +1,25 @@
-use kindle_manager::kindle_commands;
+use clap::Parser;
+use kindle_manager::KindleManager;
 
-fn main() {
-    println!("{}", kindle_commands::add(1, 2))
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    address: String,
+    #[arg(short, long, default_value_t = String::from("/mnt/us/images"))]
+    location: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let km = KindleManager::new(args.address, args.location)
+        .await
+        .expect("Failed to create KindleManager");
+
+    if let Ok(files) = km.get_files().await {
+        for file in files {
+            println!("{}", file);
+        }
+    }
 }
--- kindle_manager/src/lib.rs
+++ kindle_manager/src/lib.rs
@@ -1,16 +1,35 @@
-pub mod kindle_commands {
-    pub fn add(left: usize, right: usize) -> usize {
-        left + right
+use openssh::{KnownHosts, Session};
+
+pub struct KindleManager {
+    session: Session,
+    location: String,
+}
+
+impl KindleManager {
+    pub async fn new(address: String, location: String) -> Result<Self, openssh::Error> {
+        let session = Session::connect_mux(address, KnownHosts::Strict).await?;
+        Ok(KindleManager { session, location })
+    }
+
+    pub async fn get_files(&self) -> Result<Vec<String>, openssh::Error> {
+        let output = self
+            .session
+            .command("ls")
+            .arg(&self.location)
+            .output()
+            .await?;
+        let stdout = String::from_utf8(output.stdout).expect("Server output was not valid UTF-8");
+        let stderr = String::from_utf8(output.stderr).expect("Error output was not valid UTF-8");
+        if !stderr.is_empty() {
+            eprintln!("Kindle Manager Error: {}", stderr);
+        }
+
+        Ok(stdout
+            .split('\n')
+            .map(|s| s.to_string())
+            .filter(|s| !s.is_empty())
+            .collect())
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::kindle_commands::add;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod image_converter {}
