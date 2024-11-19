acada933c71da8259bab996f4259bec5df857ec2
João Dini <copperbrjdmdini@gmail.com>
2024-11-19 19:12:22 -0300

    feat(kindle_cli): add CLI interface for `kindle_manager` commands
    Introduced argument parsing and command handling to the CLI tool, enabling users to interact with the `kindle_manager` functions via command-line options.

--- kindle_cli/src/main.rs
+++ kindle_cli/src/main.rs
@@ -1,20 +1,50 @@
 use std::{path::PathBuf, process};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use kindle_manager::KindleManager;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+struct Cli {
     #[arg(short, long)]
     address: String,
+
     #[arg(short, long, default_value_t = String::from("/mnt/us/images"))]
     location: String,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Prepares the Kindle by disabling the screensaver and other services.
+    Prep,
+    /// Lists all files in the specified location
+    List,
+    /// Deletes a file
+    Delete { filename: String },
+    /// Pushes a file to the specified location
+    Push {
+        file_path: PathBuf,
+        filename: String,
+    },
+    /// Pulls a file from the specified location
+    Pull {
+        filename: String,
+        file_path: PathBuf,
+    },
+    /// Shows an image on screen from the specified location
+    Set { filename: String },
+    /// Prints current battery percentage
+    BatteryInfo,
+    /// Shows a debug message on screen
+    DebugPrint { message: String },
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
+    let args = Cli::parse();
     let kindle_manager = match KindleManager::new(args.address, args.location).await {
         Ok(manager) => manager,
         Err(err) => {
@@ -24,12 +54,32 @@ async fn main() {
         }
     };
 
-    println!();
-    if let Ok(battery) = kindle_manager.info_battery().await {
-        if battery < 30 {
-            debug_print(&kindle_manager, "    LOW BATTERY!").await;
+    match args.command {
+        Commands::Prep => prep(&kindle_manager).await,
+        Commands::List => list_files(&kindle_manager).await,
+        Commands::Delete { filename } => delete_file(&kindle_manager, &filename).await,
+        Commands::Push {
+            file_path,
+            filename,
+        } => push_file(&kindle_manager, &file_path, &filename).await,
+        Commands::Pull {
+            filename,
+            file_path,
+        } => pull_file(&kindle_manager, &filename, &file_path).await,
+        Commands::Set { filename } => set_image(&kindle_manager, &filename).await,
+        Commands::BatteryInfo => info_battery(&kindle_manager).await,
+        Commands::DebugPrint { message } => debug_print(&kindle_manager, &message).await,
+    }
+}
+
+async fn prep(kindle_manager: &KindleManager) {
+    match kindle_manager.prep().await {
+        Ok(_) => println!("Kindle is prepared to show images now"),
+        Err(err) => {
+            eprintln!("Failed to prepare the Kindle. Restart it before trying again");
+            eprintln!("{err}");
+            process::exit(1);
         }
-        debug_print(&kindle_manager, &format!("{battery}%")).await;
     }
 }
 
@@ -62,11 +112,8 @@ async fn delete_file(kindle_manager: &KindleManager, filename: &str) {
     }
 }
 
-async fn pull_file(kindle_manager: &KindleManager, filename: &str, path_file: &str) {
-    match kindle_manager
-        .pull_file(filename, &PathBuf::from(path_file))
-        .await
-    {
+async fn pull_file(kindle_manager: &KindleManager, filename: &str, file_path: &PathBuf) {
+    match kindle_manager.pull_file(filename, file_path).await {
         Ok(_) => println!("Pulled \"{filename}\""),
         Err(err) => {
             eprintln!("Failed to pull file");
@@ -76,11 +123,8 @@ async fn pull_file(kindle_manager: &KindleManager, filename: &str, path_file: &s
     }
 }
 
-async fn push_file(kindle_manager: &KindleManager, filename: &str, path_file: &str) {
-    match kindle_manager
-        .push_file(&PathBuf::from(path_file), filename)
-        .await
-    {
+async fn push_file(kindle_manager: &KindleManager, file_path: &PathBuf, filename: &str) {
+    match kindle_manager.push_file(file_path, filename).await {
         Ok(_) => println!("Pushed \"{filename}\""),
         Err(err) => {
             eprintln!("Failed to push file");
@@ -114,7 +158,7 @@ async fn info_battery(kindle_manager: &KindleManager) {
 
 async fn debug_print(kindle_manager: &KindleManager, text: &str) {
     match kindle_manager.debug_print(text).await {
-        Ok(battery) => println!("Printed \"{text}\""),
+        Ok(_) => println!("Printed \"{text}\""),
         Err(err) => {
             eprintln!("Failed to print debug message!");
             eprintln!("{err}");
